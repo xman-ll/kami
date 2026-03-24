d7de528c9d87abd8c854593dcddb50964ab3afcc
x-man0001 <1510651204@qq.com>
2026-03-24 10:44:27 +0800

    修改导航

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 
-import BrandLink from "@/components/brand-link";
 import { hasAdminSession } from "@/lib/auth";
 
 import "./globals.css";
@@ -23,7 +22,7 @@ export default async function RootLayout({
       <body>
         <div className="shell">
           <header className="topbar">
-            <BrandLink />
+            <span className="brand">卡密系统</span>
             <nav className="nav">
               {isAdminLoggedIn ? <Link href="/admin">后台管理</Link> : null}
               <Link href="/redeem">核销页面</Link>
